18e2f28713da5c7c33b4c98bf4d694ca1816aafe
Gary Wang <kaiyuewang123@gmail.com>
2020-02-23 11:21:40 -0800

    save

--- DTLearner.py
+++ DTLearner.py
@@ -1,64 +1,70 @@
-"""  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-A simple wrapper for linear regression.  (c) 2015 Tucker Balch  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-Note, this is NOT a correct DTLearner; Replace with your own implementation.  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-Atlanta, Georgia 30332  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-All Rights Reserved  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-Template code for CS 4646/7646  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-Georgia Tech asserts copyright ownership of this template and all derivative  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-works, including solutions to the projects assigned in this course. Students  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-and other users of this template code are advised not to share it with others  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-or to make it available on publicly viewable websites including repositories  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-such as github and gitlab.  This copyright statement should not be removed  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-or edited.  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-We do grant permission to share solutions privately with non-students such  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-as potential employers. However, sharing with other current or future  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-students of CS 7646 is prohibited and subject to being investigated as a  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-GT honor code violation.  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
------do not edit anything above this line---  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-Student Name: Tucker Balch (replace with your name)  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-GT User ID: tb34 (replace with your User ID)  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-GT ID: 900897987 (replace with your GT ID)  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-"""  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-import numpy as np  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-import warnings  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-class DTLearner(object):  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    def __init__(self, leaf_size=1, verbose = False):  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        warnings.warn("\n\n  WARNING! THIS IS NOT A CORRECT DTLearner IMPLEMENTATION! REPLACE WITH YOUR OWN CODE\n")  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        pass # move along, these aren't the drones you're looking for  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    def author(self):  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        return 'tb34' # replace tb34 with your Georgia Tech username  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    def addEvidence(self,dataX,dataY):  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        """  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        @summary: Add training data to learner  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        @param dataX: X values of data to add  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        @param dataY: the Y training values  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        """  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        # slap on 1s column so linear regression finds a constant term  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        newdataX = np.ones([dataX.shape[0],dataX.shape[1]+1])  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        newdataX[:,0:dataX.shape[1]]=dataX  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        # build and save the model  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        self.model_coefs, residuals, rank, s = np.linalg.lstsq(newdataX, dataY, rcond=None)  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    def query(self,points):  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        """  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        @summary: Estimate a set of test points given the model we built.  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        @param points: should be a numpy array with each row corresponding to a specific query.  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        @returns the estimated values according to the saved model.  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        """  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-        return (self.model_coefs[:-1] * points).sum(axis = 1) + self.model_coefs[-1]  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-if __name__=="__main__":  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    print("the secret clue is 'zzyzx'")  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
+import numpy as np
+import math
+
+class DTLearner(object):
+    def __init__(self, leaf_size = 1, verbose = False):
+        self.leaf_size = leaf_size
+        pass
+
+    def author(self):
+        return 'analwaya3'
+
+    def addEvidence(self, x, y):
+        self.model = np.array([]).reshape((0,4))
+        data = np.column_stack((x, y))
+        self.model = np.concatenate([self.model, self.createDT(data)], axis=0)
+
+    def createDT(self, data):
+        x = data[:, 0:-1]
+        y = data[:, -1:]
+        if (x.shape[0] <= self.leaf_size) or (np.all(y == y[0])):
+            split_val = y.mean()
+            return np.vstack((self.model, np.array([['leaf', split_val, np.nan, np.nan]])))
+        else:
+            max_index = self.get_index(x, y)
+            split_val = np.median(data[:, max_index])
+            lefttree_shape = (data[data[:, max_index] <= split_val]).shape[0]
+            righttree_shape = (data[data[:, max_index] > split_val]).shape[0]
+            if (lefttree_shape == data.shape[0]) or (righttree_shape == data.shape[0]):
+                split_val = np.mean(data[:, max_index])
+                leaf_val = y.mean()
+                return np.vstack((self.model, np.array([['leaf', leaf_val, np.nan, np.nan]])))
+            lefttree = self.createDT(data[data[:, max_index] <= split_val])
+            righttree = self.createDT(data[data[:, max_index] > split_val])
+            root = np.array([[max_index, split_val, 1, lefttree.shape[0] + 1]])
+            return np.vstack((self.model, root, lefttree, righttree))
+
+
+    def query(self, points):
+        decision = np.empty([points.shape[0], 1], dtype = float)
+        for index, row in enumerate(points):
+            start_index = 0
+            factor = self.model[start_index, 0]
+            while (factor != 'leaf'):
+                factor_int = int(float(factor))
+                if (row[factor_int] <= float(self.model[start_index, 1])):
+                    start_index += 1
+                else:
+                    start_index += int(float(self.model[start_index, 3]))
+                factor = self.model[start_index, 0]
+
+            decision[index, 0] = float(self.model[start_index, 1])
+
+        return decision.flatten()
+
+
+    def get_index(self, x, y):
+        corrs = abs(np.array([self.get_correlation(x_attr, y.T) for x_attr in x.T]))
+        corr_matrix = np.zeros((x.shape[1], 0))
+        for index, corr in enumerate(corrs):
+            if(not(math.isnan(corr[0, 1]))):
+                corr_matrix = np.append(corr_matrix, np.array(corr[0,1]))
+            else:
+                corr_matrix = np.append(corr_matrix, np.array([0]))
+
+        selected_attr = np.argmax(corr_matrix)
+
+        return selected_attr
+
+    def get_correlation(self, x, y):
+        return np.corrcoef(x, y)
--- gen_data.py
+++ gen_data.py
@@ -30,23 +30,36 @@ import math
   		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
 # this function should return a dataset (X and Y) that will work  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
 # better for linear regression than decision trees  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-def best4LinReg(seed=1489683273):  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    np.random.seed(seed)  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    X = np.zeros((100,2))  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    Y = np.random.random(size = (100,))*200-100  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    # Here's is an example of creating a Y from randomly generated  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    # X with multiple columns  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    # Y = X[:,0] + np.sin(X[:,1]) + X[:,2]**2 + X[:,3]**3  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    return X, Y  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
+def best4LinReg(seed=1489683273):
+    np.random.seed(seed)
+    num_rows = np.random.randint(10, 1000)
+    num_feature = np.random.randint(2, 10)
+
+    X = np.random.random((num_rows, num_feature))
+    Y = np.random.random(size = (num_rows,))
+    C = np.random.random((num_feature, ))
+    bias = np.random.random((num_rows,))
+
+    Y = np.matmul(X, C) + bias
+    return X, Y
   		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
 def best4DT(seed=1489683273):  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    np.random.seed(seed)  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    X = np.zeros((100,2))  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    Y = np.random.random(size = (100,))*200-100  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
+    np.random.seed(seed)
+    num_rows = np.random.randint(10, 1000)
+    num_feature = np.random.randint(2, 10)
+
+    X = np.random.random((num_rows, num_feature))
+    Y = np.random.random(size = (num_rows,))
+
+
+    picked_index = np.random.randint(0, num_feature)
+    Y[X[:, picked_index] < 0.5] = 1
+    Y[X[:, picked_index] > 0.5] = 0
+
     return X, Y  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
   		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
 def author():  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
-    return 'tb34' #Change this to your user ID  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
+    return 'kwang441' #Change this to your user ID
   		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
 if __name__=="__main__":  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
     print("they call me Tim.")  		  	   		     			  		 			     			  	  		 	  	 		 			  		  			
